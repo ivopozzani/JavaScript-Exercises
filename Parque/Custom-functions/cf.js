79f8b6ad6d321b70cc72c2223667ecb6f4bd3707
ivopozzani <ivo.pozzani@gmail.com>
2021-08-24 20:09:52 -0300

    Update cf.js

--- Parque/Custom-functions/cf.js
+++ Parque/Custom-functions/cf.js
@@ -1,11 +1,21 @@
 // definição da função
-function CalculateBill() {
+function CalculateBill(billAmount, taxrate) {
   // corpo da função
   console.log('rodando a função nova');
-  const total = 53 * 1.3;
+  const total = billAmount * (1 + taxrate);
   return total;
 }
 // chamada função
-const myTotal = CalculateBill();
+const myTotal = CalculateBill(100, 0.15);
 console.log(`O total da refeição foi $${myTotal}`);
-console.log(`O total da refeição foi $${CalculateBill()}`);
+console.log(`O total da refeição foi $${CalculateBill(251, 0.14)}`);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = 'Jordana') {
+  return `Olá, ${name.toUpperCase()} tudo bem?`;
+}
+
+console.log(yell(doctorize('Maria')));
