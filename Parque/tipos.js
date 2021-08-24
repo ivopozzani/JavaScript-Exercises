f76431260963f8214a0b0e6c0223857428fe9d72
ivopozzani <ivo.pozzani@gmail.com>
2021-08-24 18:35:17 -0300

    tipos

--- Parque/tipos.js
+++ Parque/tipos.js
@@ -6,3 +6,21 @@ const ultimo = `vai`;
 
 
 console.log('funciona');
+let name = `Pozzani`;
+let html = `
+<div> Olá Mundo </div>
+<p> Direto do javascript </p>
+<p> Me chamo ${name}, prazer!</p>
+`;
+
+document.body.innerHTML = html;
+console.log(html);
+
+const carro = {
+  marca: 'Fiat',
+  modelo: 'Doblô',
+  ano: 1999
+}
+
+console.log(carro.modelo)
+
