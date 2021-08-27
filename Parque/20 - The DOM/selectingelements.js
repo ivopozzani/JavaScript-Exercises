f8f518ac13ebb99bca501d6cd54ebf858f6657e9
ivopozzani <ivo.pozzani@gmail.com>
2021-08-27 18:48:34 -0300

    classes

--- Parque/20 - The DOM/selectingelements.js
+++ Parque/20 - The DOM/selectingelements.js
@@ -1,2 +1,32 @@
-const div = document.querySelector('main');
-console.log(div);
+const div = document.querySelector('#main');
+
+const h = document.querySelector('h2');
+console.dir(h);
+console.log(h.textContent);
+h.textContent = 'Mudei o texto pelo JavaScript';
+console.log(h.textContent);
+
+const hidden = document.querySelector('div span');
+console.log(hidden);
+console.log(hidden.innerHTML);
+console.log(hidden.outerHTML);
+console.log(hidden.textContent);
+
+const add = document.querySelector('.sub-items p');
+console.log(add);
+add.textContent += ' texto tbm inserido';
+add.insertAdjacentText('beforeend', ' Texto Inserido');
+
+// Totally wrong way to work with classes
+// add.classList += 'nova-classe';
+// add.className += ' new-class';
+// add.classList = 'deleted';
+
+const pic = document.querySelector('.pic');
+// pic.classList.add('rodar', 'filtrar');
+console.log(pic);
+console.log(pic.classList);
+
+document.addEventListener('click', function () {
+  pic.classList.toggle('rodar');
+});
