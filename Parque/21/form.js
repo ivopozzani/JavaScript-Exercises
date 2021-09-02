98ec2c822a6a690d0ee4901a35750fedf03b6b6e
ivopozzani <ivo.pozzani@gmail.com>
2021-09-02 19:40:46 -0300

    fisrt form

---
+++ Parque/21/form.js
@@ -0,0 +1,23 @@
+const submit = document.querySelector('form');
+// submit.addEventListener('submit', function (event) {
+//   // if (!event.currentTarget.nome.value) {
+//   //   alert('Nome é necessário');
+//   //   event.preventDefault();
+//   // }
+//   // if (!event.currentTarget.email.value) {
+//   //   alert('Email é necessário');
+//   //   event.preventDefault();
+//   // }
+//   // if (confirm('Confirma seus dados') == true) {
+//   //   alert('dados salvos con sucesso');
+//   // }
+//   event.preventDefault();
+//   console.log(event);
+//   console.log(event.form);
+
+//   console.log(event.currentTarget);
+//   console.log(event.currentTarget.nome);
+//   console.log(event.currentTarget.nome.value);
+
+//   console.log(event.currentTarget.submeter);
+// });
